0f75f62eb190a5d4d29481d3203a689458cefc6f
Koushik Mondal <darkdeavil004@gmail.com>
2023-03-04 23:17:46 +0530

    first commit

--- App.js
+++ App.js
@@ -1,20 +1,30 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { ScrollView, TextInput } from "react-native";
+import { View,Text,TouchableOpacity,StyleSheet ,Image,SafeAreaView} from "react-native";
+import styles from "./style/style";
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+export default function App(){
+  return(
+    <ScrollView>
+    <SafeAreaView style={styles.container}>
+
+      <View style={styles.view1}>
+        <Image source={{uri:"https://thecapplug.com/img/cms/94599-contact-us.gif"}} style={{ width: "100%",height:250, }}/>
+        <Text style={styles.text}>Contact Us</Text>
+      </View>
+
+      <View style={styles.view2}>
+        <TextInput style={styles.TextInput} placeholder="Name" keyboardType="default"></TextInput>
+        <TextInput style={styles.TextInput} placeholder="Email" keyboardType="default"></TextInput>
+        <TextInput style={styles.TextInput} placeholder="Phone Number" keyboardType = "number-pad"></TextInput>
+        <TextInput style={styles.TextInput3} placeholder="Massege" keyboardType="default"></TextInput>      
+      </View>
+
+      <View style={styles.view3}>
+        <TouchableOpacity style={styles.TouchableOpacity}><Text>Submit</Text></TouchableOpacity>
+      </View>
+      
+    </SafeAreaView>
+    </ScrollView>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
